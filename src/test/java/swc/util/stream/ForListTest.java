22742b4ec5618524d7e3e9611f4df1ab7c366d80
Andy-Shao <hj13035952619@126.com>
2016-01-09 18:44:05 +0800

    add ForListTest.java

---
+++ src/test/java/swc/util/stream/ForListTest.java
@@ -0,0 +1,28 @@
+package swc.util.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ForListTest {
+    private volatile List<Integer> integers;
+
+    @Before
+    public void before() {
+        this.integers = Arrays.asList(1 , 2 , 3 , 4 , 5 , 6 , 5 , 4 , 3 , 2 , 1);
+    }
+    
+    @Test
+    public void reduceTest(){
+        Optional<Integer> max = this.integers.stream().reduce(Integer::max);
+        Assert.assertThat(max.get() , Matchers.is(Integer.valueOf(6)));
+        
+        int maxInt = this.integers.stream().mapToInt(Integer::intValue).reduce(Integer.MIN_VALUE , Integer::max);
+        Assert.assertThat(maxInt , Matchers.is(6));
+    }
+}
