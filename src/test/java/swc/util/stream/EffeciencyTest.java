9647e816612df7da2a946e4c5e9126887ced9a55
Andy-Shao <hj13035952619@126.com>
2016-02-04 10:57:54 +0800

    add testing

--- src/test/java/swc/util/stream/EffeciencyTest.java
+++ src/test/java/swc/util/stream/EffeciencyTest.java
@@ -2,53 +2,34 @@ package swc.util.stream;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class EffeciencyTest {
 
+    @Ignore
     @Test
     public void forList() {
         List<Integer> numbers = Arrays.asList(1 , 2 , 3 , 4 , 5 , 6 , 7 , 8);
-        final AtomicInteger runTimes = new AtomicInteger(0);
+        long runningTime = 0L;
+        final int loopTime = 100000;
 
-        numbers.stream().filter(i -> {
-            runTimes.incrementAndGet();
-            return i < 10;
-        }).mapToInt(i -> {
-            runTimes.incrementAndGet();
-            return i.intValue();
-        }).sum();
-        System.out.println(runTimes.get());
+        runningTime = System.currentTimeMillis();
+        for (int i = 0 ; i < loopTime ; i++)
+            numbers.stream().filter(number -> number < 10).filter(number -> number > 3)
+                .filter(number -> number % 2 == 0).mapToInt(Integer::intValue).sum();
+        runningTime = System.currentTimeMillis() - runningTime;
+        System.out.println("Stream Running Time Is : " + runningTime + " millis");
 
-        runTimes.set(0);
-        numbers.stream().filter(i -> {
-            runTimes.incrementAndGet();
-            return i < 10;
-        }).filter(i -> {
-            runTimes.incrementAndGet();
-            return i > 3;
-        }).mapToInt(i -> {
-            runTimes.incrementAndGet();
-            return i.intValue();
-        }).sum();
-        System.out.println(runTimes.get());
-
-        runTimes.set(0);
-        numbers.stream().filter(i -> {
-            runTimes.incrementAndGet();
-            return i < 10;
-        }).filter(i -> {
-            runTimes.incrementAndGet();
-            return i > 3;
-        }).filter(i -> {
-            runTimes.incrementAndGet();
-            return i % 2 == 0;
-        }).mapToInt(i -> {
-            runTimes.incrementAndGet();
-            return i.intValue();
-        }).sum();
-        System.out.println(runTimes.get());
+        runningTime = System.currentTimeMillis();
+        for (int i = 0 ; i < loopTime ; i++)
+            for (int number : numbers) {
+                @SuppressWarnings("unused")
+                int sum = 0;
+                if (number < 10 && number > 3 && number % 2 == 0) sum += number;
+            }
+        runningTime = System.currentTimeMillis() - runningTime;
+        System.out.println("Handmade Loop Runnnig Time Is: " + runningTime + " millis");
     }
 }
