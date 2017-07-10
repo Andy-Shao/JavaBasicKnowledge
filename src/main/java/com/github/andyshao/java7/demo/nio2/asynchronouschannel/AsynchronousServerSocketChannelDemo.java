1798dc2c56ff8789fc20ffac40e498398b1a9a95
Andy.Shao <hj13035952619@126.com>
2017-07-10 13:15:15 +0800

    adjust code

--- src/main/java/com/github/andyshao/java7/demo/nio2/asynchronouschannel/AsynchronousServerSocketChannelDemo.java
+++ src/main/java/com/github/andyshao/java7/demo/nio2/asynchronouschannel/AsynchronousServerSocketChannelDemo.java
@@ -2,6 +2,7 @@ package com.github.andyshao.java7.demo.nio2.asynchronouschannel;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.StandardSocketOptions;
 import java.nio.channels.AsynchronousChannelGroup;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
@@ -18,18 +19,28 @@ import java.util.concurrent.Executors;
  *
  */
 public class AsynchronousServerSocketChannelDemo {
+    public static void main(String[] args) throws IOException {
+        AsynchronousServerSocketChannelDemo demo = new AsynchronousServerSocketChannelDemo();
+        demo.startAsysncSimpleServer();
+    }
 
     public void startAsysncSimpleServer() throws IOException {
+        //ChannelGroup用来管理共享资源
         AsynchronousChannelGroup group =
             AsynchronousChannelGroup.withFixedThreadPool(10 , Executors.defaultThreadFactory());
-        final AsynchronousServerSocketChannel serverChannel =
-            AsynchronousServerSocketChannel.open(group).bind(new InetSocketAddress(10080));
-        serverChannel.accept(null , new CompletionHandler<AsynchronousSocketChannel , Void>() {
+        final AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel.open(group);
+        //通过setOption配置Socket
+        server.setOption(StandardSocketOptions.SO_REUSEADDR , true);
+        server.setOption(StandardSocketOptions.SO_RCVBUF , 16 * 1024);
+        //绑定到主机，端口
+        server.bind(new InetSocketAddress(10080));
+        //等待链接，并注册CompletionHandler处理内核完成后的操作
+        server.accept(null , new CompletionHandler<AsynchronousSocketChannel , Void>() {
 
             @Override
             public void completed(AsynchronousSocketChannel ch , Void attachment) {
                 // accept the next connection
-                serverChannel.accept(null , this);
+                server.accept(null , this);
                 
                 //use clientChannel
             }
@@ -39,5 +50,7 @@ public class AsynchronousServerSocketChannelDemo {
                 //process error
             }
         });
+        
+        //因为AIO不会阻塞调用进程，因此必须让主进程阻塞，才能保持进程存活
     }
 }
